a3402a679a5333fc7a7929878884c071c5dd7a55
Bence Szatmari <Bence_Szatmari@epam.com>
2020-09-29 09:21:24 +0200

    added pgadmin db container, postgraphile postgraphile to package json

--- service/index.ts
+++ service/index.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import { postgraphile } from 'postgraphile';
 const app = express();
+
 const PORT = 3001;
-app.get('/', (req,res) => res.send('Express + TS Server is awesome'));
+
+app.use(cors())
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(
+  postgraphile(
+    "postgres://admin:secret@localhost:5432/social_media",
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      extendedErrors: ["errcode", "detail", "hint"],
+      enhanceGraphiql: true,
+    }
+  )
+)
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT} !!`);
 });
